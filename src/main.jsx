243706fc5b86c89e98d39f2c897913c7bb125782
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-10 18:48:31 -0300

    remove files & adds bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WpmApp from './WpmApp';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<WpmApp />
+	</React.StrictMode>
+);
