fea0efc838485ab39f32fb913de4fc72841d1649
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-10 18:50:11 -0300

    changes files name

---
+++ src/TypingTestApp.jsx
@@ -0,0 +1,5 @@
+function TypingTestApp() {
+	return <h1>WPM - Words per minute</h1>;
+}
+
+export default TypingTestApp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import WpmApp from './WpmApp';
-import './index.css';
+import TypingTestApp from './TypingTestApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<WpmApp />
+		<TypingTestApp />
 	</React.StrictMode>
 );
