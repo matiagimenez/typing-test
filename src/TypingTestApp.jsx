9d3231090de5f74499a52a8690485a22af5c8571
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-10 19:48:16 -0300

    Finished

--- src/TypingTestApp.jsx
+++ src/TypingTestApp.jsx
@@ -1,5 +1,58 @@
+import { Form } from './Form';
+import randomWord from 'random-words';
+import { useEffect, useState } from 'react';
+
 function TypingTestApp() {
-	return <h1>WPM - Words per minute</h1>;
+	const [word, setWord] = useState(randomWord());
+	const [count, setCount] = useState(0);
+	const [time, setTime] = useState(60);
+
+	useEffect(() => {
+		if (time > 0) {
+			const timer = setTimeout(() => {
+				setTime(time - 1);
+			}, 1000);
+
+			return () => {
+				clearTimeout(timer);
+			};
+		}
+	}, [time]);
+
+	function handleReset() {
+		setTime(60);
+		setWord(randomWord());
+		setCount(0);
+	}
+
+	function handleIncrementCount(inputValue) {
+		if (inputValue.trim() === word) {
+			setCount(count + word.length);
+			setWord(randomWord());
+		}
+	}
+
+	return (
+		<div className='container'>
+			<h1 className='word'>{word}</h1>
+			<h2 className='count'>
+				{time > 0 ? `Characters typed: ${count}` : `Score: ${count}`}
+			</h2>
+			{time > 0 ? (
+				<>
+					<h4 className='time'>⌛ {time} seconds </h4>
+					<Form handleIncrementCount={handleIncrementCount} />
+				</>
+			) : (
+				<>
+					<h4 className='time'>GAME OVER</h4>
+					<button className='play' onClick={handleReset}>
+						Play Again
+					</button>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default TypingTestApp;
